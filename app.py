2f853e402b5ed7f23e4d2fdc053321e718a01907
Harshita Mohanram <harshitamohan137@gmail.com>
2023-02-25 18:24:56 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
+import json
+import pickle
 
-import json 
-import pickle 
-
-from flask import Flask, request,app,jsonify,url_for,render_template
+from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
-import pandas as pd 
+import pandas as pd
 
 app=Flask(__name__)
-##To load the model
-
+## Load the model
 regmodel=pickle.load(open('regmodel.pkl','rb'))
 scalar=pickle.load(open('scaling.pkl','rb'))
 @app.route('/')
 def home():
     return render_template('home.html')
-
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
@@ -25,6 +22,16 @@ def predict_api():
     output=regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
